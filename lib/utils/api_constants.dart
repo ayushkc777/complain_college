99fafcf387791b2c78b870379cea7456c2dd11d5
ayushkc777 <ayushkc777@gmail.com>
2026-01-29 20:20:18 +0545

    Sprint 4: add api constants

---
+++ lib/utils/api_constants.dart
@@ -0,0 +1,5 @@
+class ApiConstants {
+  static const String baseUrl = "http://localhost:3000/api/v1";
+  static const String register = "/students";
+  static const String login = "/students/login";
+}
