6e1388aaf3868e5da8aea48be57e3ef3ada131d0
ayushkc777 <ayushkc777@gmail.com>
2026-01-29 19:40:52 +0545

    Sprint 2: add dashboard shell

---
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import '../../utils/colors.dart';
+import '../home/home_screen.dart';
+import 'tabs/complaints_tab.dart';
+import 'tabs/profile_tab.dart';
+import 'widgets/app_drawer.dart';
+
+class DashboardScreen extends StatefulWidget {
+  const DashboardScreen({super.key});
+
+  @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = const [
+    HomeScreen(),
+    ComplaintsTab(),
+    ProfileTab(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: AppDrawer(
+        onSelect: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+          Navigator.pop(context);
+        },
+      ),
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _pages,
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: AppColors.primaryBlue,
+        onPressed: () {},
+        child: const Icon(Icons.add, color: Colors.white),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12.withOpacity(0.06),
+              blurRadius: 8,
+              offset: const Offset(0, -2),
+            )
+          ],
+        ),
+        child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          backgroundColor: Colors.white,
+          elevation: 0,
+          selectedItemColor: AppColors.primaryBlue,
+          unselectedItemColor: AppColors.textGrey,
+          showUnselectedLabels: true,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.list_alt_outlined),
+              label: "Complaints",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline),
+              label: "Profile",
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
